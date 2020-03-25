5c5a27e13d0886b455a2ff22d7eff612392d4493
Robel Solomon <44929813+rosolomo@users.noreply.github.com>
2020-03-24 20:58:56 -0400

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,8 +1,16 @@
 import sys, os
+import subprocess
 
+f= open("links.txt",r)
+urls = f.readline()
+print(urls)
 
-f = open("links.txt", "r")
-f=f.read()
-links = [x for x in f]
-url=os.system('youtube-dl -g '+ links[0])
-os.system('omxplayer -o hdmi '+url)
+quit()
+url='https://www.youtube.com/watch?v=vw61gCe2oqI'
+yta = ['youtube-dl', '-g',url, '--restrict-filenames']
+yt = subprocess.Popen(yta,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+(res,err) = yt.communicate()
+if res and not err:
+   print(res)
+
+ os.system('omxplayer -o hdmi '+str(res))
