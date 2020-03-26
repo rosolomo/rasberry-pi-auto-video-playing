bd30e75ee4cf088dc9204e218f1a400d7ae1927c
Robel Solomon <44929813+rosolomo@users.noreply.github.com>
2020-03-26 18:19:36 -0400

    Update play.py

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ from autoplay import play
 import os
 cwd = os.getcwd()
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print(cwd)
-print(dir_path)
+print("cwd",cwd)
+print("dirp",dir_path)
 
 play()
